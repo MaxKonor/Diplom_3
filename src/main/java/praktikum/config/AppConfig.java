d00bc2dcbf957d4317c1df837c671ac9b1a8363e
Maksim Kondrashov <konor.max@mail.ru>
2023-04-28 11:02:25 +0300

    added  class AppConfig

---
+++ src/main/java/praktikum/config/AppConfig.java
@@ -0,0 +1,7 @@
+package praktikum.config;
+
+public class AppConfig {
+    public static final String URL_LOGIN = "https://stellarburgers.nomoreparties.site/login";
+    public static final String URL_MAIN = "https://stellarburgers.nomoreparties.site/";
+    public static final String URL_REGISTER = "https://stellarburgers.nomoreparties.site/register";
+}
