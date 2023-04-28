559e7ab7eeaccf0d5f926d5f3daf4b2b911e8f25
Maksim Kondrashov <konor.max@mail.ru>
2023-04-28 11:09:32 +0300

    added  class Credentials, RandomGenerator, User

---
+++ src/main/java/praktikum/userData/User.java
@@ -0,0 +1,41 @@
+package praktikum.userData;
+
+import io.qameta.allure.Step;
+import praktikum.pages.LoginPage;
+import praktikum.pages.RegisterPage;
+
+import static com.codeborne.selenide.Selenide.page;
+
+public class User {
+
+    LoginPage loginPage = page(LoginPage.class);
+    RegisterPage registerPage = page(RegisterPage.class);
+
+    public String userName;
+    public String userEmail;
+    public String userPassword;
+
+    public User(String userName, String userEmail, String userPassword) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+    }
+
+    @Step("Регистрация пользователя")
+    public void RegistrationUser() {
+        Credentials credentials = new Credentials(userName, userEmail, userPassword);
+        registerPage.setUserNameField(credentials.userName);
+        registerPage.setUserEmailField(credentials.userEmail);
+        registerPage.setUserPasswordField(credentials.userPassword);
+        registerPage.clickToRegisterButton();
+    }
+
+    @Step("Авторизация пользователя")
+    public void LogInUser() {
+        Credentials credentials = new Credentials(userName, userEmail, userPassword);
+        loginPage.setEmailField(credentials.userEmail);
+        loginPage.setPasswordField(credentials.userPassword);
+        loginPage.clickToLoginButton();
+
+    }
+}
