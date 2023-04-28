d488f7af8290e2cecf8c5834487d73d3785ff06a
Maksim Kondrashov <konor.max@mail.ru>
2023-04-28 11:21:07 +0300

    added  test classes BurgerConstructorYandexTest, UserAccountYandexTest, UserLogInYandexTest, UserRegistrationYandexTest and BaseYandexTest

---
+++ src/test/java/praktikum/yandex/BaseYandexTest.java
@@ -0,0 +1,12 @@
+package praktikum.yandex;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.Before;
+
+public class BaseYandexTest {
+    @Before
+    public void initYandex() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        Configuration.browserBinary = "C:/Users/Администратор/AppData/Local/Yandex/YandexBrowser/Application/browser.exe";
+    }
+}
