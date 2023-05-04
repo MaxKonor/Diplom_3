f4d14b258ed9792bd7a7d90db5150952bc4cff2d
Maksim Kondrashov <konor.max@mail.ru>
2023-05-04 16:24:17 +0300

    added allure report

--- src/main/java/praktikum/extentions/WebDriverFactory.java
+++ src/main/java/praktikum/extentions/WebDriverFactory.java
@@ -9,7 +9,7 @@ public class WebDriverFactory {
     private static final String YANDEX_BROWSER_PATH = "C:\\Users\\Администратор\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe";
 
     public static void initWebDriver() {
-        //System.setProperty("browser","yandex"); //запуск локально Chrome// или yandex или ("browser","chrome")
+        //System.setProperty("browser","chrome"); //запуск локально Chrome// или yandex или ("browser","chrome")
         String browser = System.getProperty("browser"); //запуск через терминал mvn clean test -Dbrowser=yandex
                                                                          // или mvn clean test -Dbrowser=chrome
 
